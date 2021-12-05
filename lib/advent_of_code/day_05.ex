76b615134bbfdde73e990aa95fbdda6b7ee063d5
Lukas N <niederbergerlukas@gmail.com>
2021-12-05 00:31:35 -0800

    Day 5 Parts 1 & 2 Complete

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,9 +1,74 @@
 defmodule AdventOfCode.Day05 do
-  def part1(args) do
-    args
+  alias __MODULE__.Parser
+
+  def part1(input) do
+    input
+    |> Parser.parse_input()
+    |> Enum.filter(fn {{x1, y1}, {x2, y2}} ->
+      x1 == x2 or y1 == y2
+    end)
+    |> Enum.reduce(%{}, fn {{x1, y1}, {x2, y2}}, acc ->
+      for x <- x1..x2, y <- y1..y2, reduce: acc do
+        acc -> Map.update(acc, {x, y}, 1, &(&1 + 1))
+      end
+    end)
+    |> Map.values()
+    |> Enum.count(&(&1 >= 2))
   end
 
-  def part2(args) do
-    args
+  def part2(input) do
+    input
+    |> Parser.parse_input()
+    |> Enum.filter(fn {{x1, y1}, {x2, y2}} = coords ->
+      x1 == x2 or y1 == y2 or at_45_degrees?(coords)
+    end)
+    |> mark_lines()
+    |> Map.values()
+    |> Enum.count(&(&1 >= 2))
+  end
+
+  defp mark_lines(coords) do
+    Enum.reduce(coords, %{}, fn {{x1, y1}, {x2, y2}}, acc ->
+      steps = max(abs(x2 - x1), abs(y2 - y1))
+      dx = div(x2 - x1, steps)
+      dy = div(y2 - y1, steps)
+
+      Stream.iterate({x1, y1}, fn {x, y} -> {x + dx, y + dy} end)
+      |> Stream.take(steps + 1)
+      |> Enum.to_list()
+      |> Enum.reduce(acc, fn point, inner_acc -> Map.update(inner_acc, point, 1, &(&1 + 1)) end)
+    end)
+  end
+
+  def at_45_degrees?({{x1, y1}, {x2, y2}}) do
+    rise = y2 - y1
+    run = x2 - x1
+
+    slope = rise / run
+    # IO.puts("(#{x1},#{y1}),(#{x2},#{y2}) -> #{slope}")
+    abs(slope) == 1
+  end
+
+  defmodule Parser do
+    def parse_input(input) do
+      input
+      |> String.trim()
+      |> String.split("\n", trim: true)
+      |> Enum.map(&parse_line/1)
+    end
+
+    def parse_line(line) do
+      line
+      |> String.split(" -> ", trim: true)
+      |> Enum.map(&parse_coords/1)
+      |> List.to_tuple()
+    end
+
+    def parse_coords(coords) do
+      coords
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+      |> List.to_tuple()
+    end
   end
 end
--- test/advent_of_code/day_05_test.exs
+++ test/advent_of_code/day_05_test.exs
@@ -3,19 +3,21 @@ defmodule AdventOfCode.Day05Test do
 
   import AdventOfCode.Day05
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input =
+      "0,9 -> 5,9\n8,0 -> 0,8\n9,4 -> 3,4\n2,2 -> 2,1\n7,0 -> 7,4\n6,4 -> 2,0\n0,9 -> 2,9\n3,4 -> 1,4\n0,0 -> 8,8\n5,5 -> 8,2\n"
+
     result = part1(input)
 
-    assert result
+    assert result == 5
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input =
+      "0,9 -> 5,9\n8,0 -> 0,8\n9,4 -> 3,4\n2,2 -> 2,1\n7,0 -> 7,4\n6,4 -> 2,0\n0,9 -> 2,9\n3,4 -> 1,4\n0,0 -> 8,8\n5,5 -> 8,2\n"
+
     result = part2(input)
 
-    assert result
+    assert result == 12
   end
 end
