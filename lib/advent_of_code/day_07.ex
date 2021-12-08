9beb0bfb4a2556e63981e6fd222cff2d4d9296f4
Lukas N <niederbergerlukas@gmail.com>
2021-12-07 17:04:40 -0800

    Day 7 Parts 1 & 2 Complete

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,9 +1,48 @@
 defmodule AdventOfCode.Day07 do
-  def part1(args) do
-    args
+  def part1(input) do
+    numbers =
+      input
+      |> parse()
+
+    {min, max} = Enum.min_max(numbers)
+
+    min..max
+    |> Enum.map(fn destination ->
+      numbers
+      |> Enum.map(&calculate_fuel_1(&1, destination))
+      |> Enum.sum()
+    end)
+    |> Enum.min()
   end
 
-  def part2(args) do
-    args
+  def part2(input) do
+    numbers =
+      input
+      |> parse()
+
+    {min, max} = Enum.min_max(numbers)
+
+    min..max
+    |> Enum.map(fn destination ->
+      numbers
+      |> Enum.map(&calculate_fuel_2(&1, destination))
+      |> Enum.sum()
+    end)
+    |> Enum.min()
+  end
+
+  defp parse(input) do
+    input
+    |> String.trim()
+    |> String.split(",", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp calculate_fuel_1(origin, destination) do
+    abs(destination-origin)
+  end
+
+  defp calculate_fuel_2(origin, destination) do
+    1..abs(destination-origin) |> Enum.sum()
   end
 end
